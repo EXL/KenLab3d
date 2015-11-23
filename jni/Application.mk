199ae2a692cfbad4b41112f519770b77ff96e999
EXL <exlmotodev@gmail.com>
2015-11-24 04:39:35 +0600

    Update Android Project Files

--- jni/Application.mk
+++ jni/Application.mk
@@ -5,3 +5,4 @@
 
 #APP_ABI := armeabi armeabi-v7a x86
 APP_ABI := armeabi
+APP_PLATFORM := android-9
---
+++ src/ru/exlmoto/kenlab3d/KenLab3DActivity.java
@@ -0,0 +1,9 @@
+package ru.exlmoto.kenlab3d;
+
+import org.libsdl.app.SDLActivity;
+
+/*
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class KenLab3DActivity extends SDLActivity { }
